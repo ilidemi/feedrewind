2c064666ef54e77e3ba20a781481c05e86daa7cc
Ilia Demianenko <ilia.demianenko@gmail.com>
2023-06-02 19:24:16 -0700

    Chi, ignore exe

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module feedrewind
 
 go 1.19
 
+require github.com/go-chi/chi/v5 v5.0.8
+
 require github.com/quasilyte/go-ruleguard/dsl v0.3.22 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	fmt.Println("I am FeedRewind")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("FeedRewind"))
+		if err != nil {
+			panic(err)
+		}
+	})
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		panic(err)
+	}
 }
