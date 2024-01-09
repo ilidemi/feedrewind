869362814985432bef54ddfc0dee8d5e01affc0b
Ilia Demianenko <ilia.demianenko@gmail.com>
2024-01-08 21:00:05 -0800

    No test

--- config/config.go
+++ config/config.go
@@ -44,8 +44,6 @@ func (c DBConfig) DSN() string {
 	password := ""
 	if c.Password != nil {
 		password = fmt.Sprintf(" password=%s", *c.Password)
-		// TODO remove
-		fmt.Printf("DB password length: %d\n", len(password))
 	}
 	return fmt.Sprintf("user=%s%s host=%s port=%d dbname=%s", c.User, password, c.Host, c.Port, c.DBName)
 }
