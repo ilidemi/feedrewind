e9315f47a68a1de87e0b30ce365dec7ed6cbcbf2
Ilia Demianenko <ilia.demianenko@gmail.com>
2024-01-08 21:24:04 -0800

    test

--- jobs/dispatch_amplitude_job.go
+++ jobs/dispatch_amplitude_job.go
@@ -53,6 +53,8 @@ func DispatchAmplitudeJob_Perform(ctx context.Context, conn *pgw.Conn, isManual 
 	failedCount := 0
 	for i, productEvent := range eventsToDispatch {
 		if err := ctx.Err(); err != nil {
+			// TODO remove
+			logger.Info().Err(err).Msg("Context error")
 			return err
 		}
 
