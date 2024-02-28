c627db4969625b2146fae99b48cc80ff6e4dbda6
Ilia Demianenko <ilia.demianenko@gmail.com>
2024-02-28 14:14:37 -0800

    Compare parsed feeds rather than raw

--- models/blog.go
+++ models/blog.go
@@ -162,23 +162,9 @@ func Blog_CreateOrUpdate(
 		return nil, err
 	}
 
-	row := tx.QueryRow(`
-		select same_hosts, expect_tumblr_paths from blog_canonical_equality_configs
-		where blog_id = $1
-	`, blog.Id)
-	var sameHostsSlice []string
-	var expectTumblrPaths bool
-	err = row.Scan(&sameHostsSlice, &expectTumblrPaths)
+	curiEqCfg, err := BlogCanonicalEqualityConfig_Get(tx, blog.Id)
 	if err != nil {
 		return nil, err
-	}
-	sameHosts := make(map[string]bool)
-	for _, sameHost := range sameHostsSlice {
-		sameHosts[sameHost] = true
-	}
-	curiEqCfg := &crawler.CanonicalEqualityConfig{
-		SameHosts:         sameHosts,
-		ExpectTumblrPaths: expectTumblrPaths,
 	}
 
 	rows, err = tx.Query(`select url from blog_discarded_feed_entries where blog_id = $1`, blog.Id)
@@ -824,4 +810,29 @@ func BlogPostCategory_GetNamePostsCountById(
 	`, categoryId)
 	err = row.Scan(&name, &postsCount)
 	return name, postsCount, err
+}
+
+// BlogCanonicalEqualityConfig
+
+func BlogCanonicalEqualityConfig_Get(
+	tx pgw.Queryable, blogId BlogId,
+) (*crawler.CanonicalEqualityConfig, error) {
+	row := tx.QueryRow(`
+		select same_hosts, expect_tumblr_paths from blog_canonical_equality_configs
+		where blog_id = $1
+	`, blogId)
+	var sameHostsSlice []string
+	var expectTumblrPaths bool
+	err := row.Scan(&sameHostsSlice, &expectTumblrPaths)
+	if err != nil {
+		return nil, err
+	}
+	sameHosts := make(map[string]bool)
+	for _, sameHost := range sameHostsSlice {
+		sameHosts[sameHost] = true
+	}
+	return &crawler.CanonicalEqualityConfig{
+		SameHosts:         sameHosts,
+		ExpectTumblrPaths: expectTumblrPaths,
+	}, nil
 }
