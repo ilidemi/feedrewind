1e5c8f3b7f0b7a8b61f92d2846b66cebd4831fe0
Ilia Demianenko <ilia.demianenko@gmail.com>
2024-06-27 20:07:21 -0700

    Update timezones, warn when they go more than 6 months out of date

---
+++ db/migrations/20240627193236_CodeMaintenanceJob.go
@@ -0,0 +1,29 @@
+package migrations
+
+import (
+	"feedrewind/db/pgw"
+	"feedrewind/util/schedule"
+)
+
+type CodeMaintenanceJob struct{}
+
+func init() {
+	registerMigration(&CodeMaintenanceJob{})
+}
+
+func (m *CodeMaintenanceJob) Version() string {
+	return "20240627193236"
+}
+
+var CodeMaintenanceJob_PerformAtFunc func(pgw.Queryable, schedule.Time) error
+
+func (m *CodeMaintenanceJob) Up(tx *Tx) {
+	err := CodeMaintenanceJob_PerformAtFunc(tx.impl, schedule.UTCNow())
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (m *CodeMaintenanceJob) Down(tx *Tx) {
+	tx.MustDeleteJobByName("CodeMaintenanceJob")
+}
