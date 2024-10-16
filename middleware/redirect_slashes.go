415b282acf6795ca2ca82d1a7b30aba1193ee5f3
Ilia Demianenko <ilia.demianenko@gmail.com>
2024-10-16 15:01:23 -0700

    Fix redirect slashes

---
+++ middleware/redirect_slashes.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func RedirectSlashes(excludePrefix string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			var path string
+			rctx := chi.RouteContext(r.Context())
+			if rctx != nil && rctx.RoutePath != "" {
+				path = rctx.RoutePath
+			} else {
+				path = r.URL.Path
+			}
+			if len(path) > 1 && path[len(path)-1] == '/' &&
+				!(strings.HasPrefix(path, excludePrefix) && path != excludePrefix) {
+
+				if r.URL.RawQuery != "" {
+					path = fmt.Sprintf("%s?%s", path[:len(path)-1], r.URL.RawQuery)
+				} else {
+					path = path[:len(path)-1]
+				}
+				redirectURL := fmt.Sprintf("//%s%s", r.Host, path)
+				http.Redirect(w, r, redirectURL, 301)
+				return
+			}
+			next.ServeHTTP(w, r)
+		}
+		return http.HandlerFunc(fn)
+	}
+}
