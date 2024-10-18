4835c44320360f0db7f3f8a3bdcdeff5a1309c23
Ilia Demianenko <ilia.demianenko@gmail.com>
2024-10-18 15:30:08 -0700

    Maintenance Job

--- jobs/maintenance_job.go
+++ jobs/maintenance_job.go
@@ -3,7 +3,10 @@ package jobs
 import (
 	"context"
 	"errors"
+	"feedrewind/config"
+	"feedrewind/db/migrations"
 	"feedrewind/db/pgw"
+	"feedrewind/models"
 	"feedrewind/oops"
 	"feedrewind/util/schedule"
 	"strings"
@@ -17,25 +20,87 @@ import (
 
 func init() {
 	registerJobNameFunc(
-		"CheckStaleStripeJob",
+		"MaintenanceJob",
 		func(ctx context.Context, id JobId, pool *pgw.Pool, args []any) error {
 			if len(args) != 0 {
 				return oops.Newf("Expected 0 args, got %d: %v", len(args), args)
 			}
 
-			return CheckStaleStripeJob_Perform(ctx, pool)
+			return MaintenanceJob_Perform(ctx, pool)
 		},
 	)
+	migrations.MaintenanceJob_PerformAtFunc = MaintenanceJob_PerformAt
 }
 
-func CheckStaleStripeJob_PerformAt(qu pgw.Queryable, runAt schedule.Time) error {
-	return performAt(qu, runAt, "CheckStaleStripeJob", defaultQueue)
+func MaintenanceJob_PerformAt(qu pgw.Queryable, runAt schedule.Time) error {
+	return performAt(qu, runAt, "MaintenanceJob", defaultQueue)
 }
 
-func CheckStaleStripeJob_Perform(ctx context.Context, pool *pgw.Pool) error {
+var warnedActiveUserIds = map[models.UserId]bool{}
+
+func MaintenanceJob_Perform(ctx context.Context, pool *pgw.Pool) error {
 	logger := pool.Logger()
-	utcNow := schedule.UTCNow()
 
+	//
+	// Duplicated PublishPostsJob
+	//
+	rows, err := pool.Query(`
+		select array_agg(id) as ids, (
+			select regexp_matches(handler, E'arguments:\n  - ([0-9]+)')
+		)[1] as user_id
+		from delayed_jobs
+		where handler like '%PublishPostsJob%'
+		group by user_id
+		having count(*) > 1
+	`)
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		var ids []int64
+		var userId string
+		err := rows.Scan(&ids, &userId)
+		if err != nil {
+			return err
+		}
+		logger.Warn().Msgf("User %s has duplicated PublishPostsJob: %v", userId, ids)
+	}
+	if err := rows.Err(); err != nil {
+		return err
+	}
+
+	//
+	// Missing PublishPostsJob
+	//
+	rows, err = pool.Query(`
+		select user_id from user_settings
+		where delivery_channel is not null
+			and user_id not in (
+				select (regexp_matches(handler, E'arguments:\n  - ([0-9]+)'))[1]::bigint from delayed_jobs
+				where handler like '%PublishPostsJob%'
+			)
+	`)
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		var userId string
+		err := rows.Scan(&userId)
+		if err != nil {
+			return err
+		}
+		logger.Warn().Msgf("User %s doesn't have a PublishPostsJob", userId)
+	}
+	if err := rows.Err(); err != nil {
+		return err
+	}
+
+	//
+	// Check stale stripe
+	//
+	utcNow := schedule.UTCNow()
 	subscriptionCutoff := utcNow.Add(-24 * time.Hour)
 	var subscriptionIds []string
 	//nolint:exhaustruct
@@ -98,7 +163,7 @@ func CheckStaleStripeJob_Perform(ctx context.Context, pool *pgw.Pool) error {
 	tokenCutoff := utcNow.Add(-7 * 24 * time.Hour)
 	row := pool.QueryRow(`select count(1) from stripe_subscription_tokens where created_at < $1`, tokenCutoff)
 	var staleCount int
-	err := row.Scan(&staleCount)
+	err = row.Scan(&staleCount)
 	if err != nil {
 		return err
 	}
@@ -151,9 +216,42 @@ func CheckStaleStripeJob_Perform(ctx context.Context, pool *pgw.Pool) error {
 		)
 	}
 
-	hourFromNow := utcNow.Add(time.Hour)
-	runAt := hourFromNow.BeginningOfHour()
-	err = CheckStaleStripeJob_PerformAt(pool, runAt)
+	//
+	// Check for overactive users
+	//
+	rows, err = pool.Query(`
+		select user_id, (select email from users where id = user_id) count(1) as count from subscriptions
+		where user_id is not null
+		group by user_id, email
+		having count >= 30
+		order by count desc
+	`)
+	if err != nil {
+		return err
+	}
+	for rows.Next() {
+		var userId models.UserId
+		var email string
+		var count int64
+		err := rows.Scan(&userId, &email, &count)
+		if err != nil {
+			return err
+		}
+		if strings.HasSuffix(email, "@feedrewind.com") ||
+			strings.HasSuffix(email, "@bounce-testing.postmarkapp.com") ||
+			config.Cfg.AdminUserIds[int64(userId)] ||
+			warnedActiveUserIds[userId] {
+			continue
+		}
+
+		logger.Warn().Msgf("User %d has many subscriptions: %d", userId, count)
+		warnedActiveUserIds[userId] = true
+	}
+
+	hourFromNow := schedule.UTCNow().Add(time.Hour)
+	// Stagger from the PublishPostsJobs that run on the hour
+	runAt := hourFromNow.BeginningOfHour().Add(30 * time.Minute)
+	err = MaintenanceJob_PerformAt(pool, runAt)
 	if err != nil {
 		return err
 	}
